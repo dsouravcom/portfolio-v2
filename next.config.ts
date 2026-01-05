26ea6bcef5d2037b9dfd2f5578d5e4abdad6a814
sourav dutta <souravduttasd2@gmail.com>
2026-01-05 20:06:55 +0530

    added security headers.

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,35 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    headers: async () => {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "strict-origin-when-cross-origin",
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "camera=(), microphone=(), geolocation=()",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sitebehaviour-cdn.fra1.cdn.digitaloceanspaces.com; style-src 'self' 'unsafe-inline' https://api.fontshare.com; font-src 'self' data: https://cdn.fontshare.com; img-src 'self' data: blob: https://res.cloudinary.com https://img.icons8.com https://caption.dsourav.com; connect-src 'self' https://sitebehaviour-cdn.fra1.cdn.digitaloceanspaces.com;",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
